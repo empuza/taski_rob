cf08c4b83b3c100b26b60456ee75e5a026fab21d
Marta Krajewska <marta.krajewska@polcode.net>
2020-03-26 11:40:46 +0100

    Little fix of config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'refresh/create'
   post 'signup', controller: :signup, action: :create
   post 'signin', controller: :signin, action: :create
   post 'refresh', controller: :refresh, action: :create
