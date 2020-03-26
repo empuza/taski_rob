25877b0ea80753c562ba1d5fc878de175cb30572
Marta Krajewska <marta.krajewska@polcode.net>
2020-03-27 00:25:48 +0100

    Adding BootstrapVue and basic configuration of the frontend project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 /* eslint-disable no-new */
 new Vue({
