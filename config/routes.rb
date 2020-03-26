7a4afa3aaa5459eea465937c116ace780bfe2387
Marta Krajewska <marta.krajewska@polcode.net>
2020-03-26 23:00:04 +0100

    Adding Signout action (destroy)to SigninController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'api/tasks#index'
+
   post 'signup', controller: :signup, action: :create
   post 'signin', controller: :signin, action: :create
+  delete 'signin', controller: :signin, action: :destroy
   post 'refresh', controller: :refresh, action: :create
 
   namespace :api do
--- spec/controllers/signin_controller_spec.rb
+++ spec/controllers/signin_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SigninController, type: :controller do
+  let(:user) { create(:user) }
+
   describe 'POST #create' do
     let(:password) { 'password' }
-    let(:user) { create(:user) }
     let(:user_params) { { email: user.email, password: password } }
 
     it 'returns http success' do
@@ -17,5 +18,32 @@ RSpec.describe SigninController, type: :controller do
       post :create, params: { email: user.email, password: 'incorrect' }
       expect(response).to have_http_status(401)
     end
+  end
+
+  describe 'logout DELETE #destroy' do
+    context 'failure' do
+      it 'returns unauthorized http status' do
+        delete :destroy
+        expect(response).to have_http_status(401)
+      end
+    end
+    # context 'success' do
+    #   it 'returns http success with valid tokens' do
+    #     payload = { user_id: user.id }
+    #
+    #     session = JWTSessions::Session.new(
+    #       payload: payload,
+    #       refresh_by_access_allowed: true
+    #     )
+    #
+    #     tokens = session.login
+    #     request.cookies[JWTSessions.access_cookie] = tokens[:access]
+    #     request.headers[JWTSessions.csrf_header] = tokens[:csrf]
+    #
+    #     delete :destroy
+    #     expect(response).to have_http_status(200)
+    #     expect(response_json).to eq('ok')
+    #   end
+    # end
   end
 end
