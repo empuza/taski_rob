10d9fb2c5189cfe24de200b47d209d4d0ca882ac
Marta Krajewska <marta.krajewska@polcode.net>
2020-03-26 11:38:54 +0100

    Adding Refresh controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'refresh/create'
   post 'signup', controller: :signup, action: :create
   post 'signin', controller: :signin, action: :create
+  post 'refresh', controller: :refresh, action: :create
 end
