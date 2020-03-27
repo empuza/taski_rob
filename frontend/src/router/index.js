bfa9478e9e13ced8294d0e01d46ef4c5cc142780
Marta Krajewska <marta.krajewska@polcode.net>
2020-03-27 02:04:27 +0100

    Adding Tasks component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Signin from '@/components/Signin'
 import Signup from '@/components/Signup'
+import Tasks from '@/components/Tasks'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -16,6 +18,11 @@ export default new Router({
       path: '/signup',
       name: 'Signup',
       component: Signup
+    },
+    {
+      path: '/tasks',
+      name: 'Tasks',
+      component: Tasks
     }
   ]
 })
