733fa33b7556fb2a9b4c12ce995fd949a84d1df2
Marta Krajewska <marta.krajewska@polcode.net>
2020-03-27 01:02:42 +0100

    Adding rest of the needed libraries - Axios, Vuex and Store and configuring Axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,15 +6,19 @@ import router from './router'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueAxios from 'vue-axios'
+import axios from './backend/axios'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  axios,
   components: { App },
   template: '<App/>'
 })
